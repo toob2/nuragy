7b8ebda4cd0e363d781b7ed6eae812dc100574c4
tomas obdrzalek <obdrzalek.tom@gmail.com>
2018-06-20 00:43:43 +0100

    sliderjs2

--- app/js/slide.js
+++ app/js/slide.js
@@ -1,1 +1,11 @@
-const slides = document.querySelectorAll('.slide-item');
+const slides = document.querySelectorAll('.slide__item');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 3000);
+
+console.log(slides.length);
+
+function nextSlide(){
+   slides[currentSlide].className = 'slide__item';
+   currentSlide = (currentSlide+1)%slides.length;
+   slides[currentSlide].classList.add('slide__item--showing');
+}
