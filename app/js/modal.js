4311695e41161df498bb2fd56b05ad3dcf8ffd65
tomas obdrzalek <obdrzalek.tom@gmail.com>
2018-06-24 11:39:58 +0100

    modalfunctions

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,12 +1,28 @@
-const buttonA = document.querySelector('.btn--hire');
-let modalCont = document.querySelector('.modal')
+const btnHire = document.querySelector('.btn--hire');
+let modal = document.querySelector('.modal--closed');
+let modalOverlay = document.querySelector('.modal-overlay--closed');
+const body = document.querySelector('body');
 
-function modal(){
-   if (modalCont.className === "modal")
-      {modalCont.className = "modal__unfold";}
-   else {
-      modalCont.className = "modal";}
-   }
+function modalUnfold(){
+   modal.classList.toggle('modal--closed')
+   modal.classList.toggle('modal--active')
+   modalOverlay.classList.toggle('modal-overlay--closed')
+   modalOverlay.classList.toggle('modal-overlay--active')
+   body.classList.toggle('body--modal')   
+};
 
-buttonA.addEventListener('click', modal);
+function modalClose(){
+   modal.classList.toggle('modal--active')
+   modal.classList.toggle('modal--closed')
+   modalOverlay.classList.toggle('modal-overlay--active')
+   modalOverlay.classList.toggle('modal-overlay--closed')
+   body.classList.toggle('body--modal')
+};
+
+btnHire.addEventListener('click', modalUnfold);
+
+modalOverlay.addEventListener('click', modalClose);
+
+
+
 
