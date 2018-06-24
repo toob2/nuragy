020483a1f884a5e7970849cdd41d32d987bbeb82
tomas obdrzalek <obdrzalek.tom@gmail.com>
2018-06-24 20:44:15 +0100

    navlinksworking

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,6 +1,8 @@
 const toggler = document.querySelector('.nav__toggler');
 const navList = document.querySelector('.nav__list');
 const burger  = document.querySelector('.nav__burger');
+const navLinks = document.querySelectorAll('.nav__link');
+const navArr = Array.apply(null, navLinks);
 
 function navToggle(){
   navList.classList.toggle('nav__list');
@@ -9,6 +11,23 @@ function navToggle(){
 };
 
 toggler.addEventListener('click', navToggle);
+
+function navf(){
+  toggler.click();
+};
+
+navArr.forEach(function(a){
+  a.addEventListener('click', function(){
+    navf();
+  }, 
+    false);
+});
+
+
+
+
+
+
 
 
 
--- app/js/slide.js
+++ app/js/slide.js
@@ -2,8 +2,6 @@ const slides = document.querySelectorAll('.slide__item');
 let currentSlide = 0;
 let slideInterval = setInterval(nextSlide, 5500);
 
-console.log(slides.length);
-
 function nextSlide(){
    slides[currentSlide].className = 'slide__item';
    currentSlide = (currentSlide+1)%slides.length;
