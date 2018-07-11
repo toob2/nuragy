416e96625a7419ad129015e79889c3f10a465bd1
tomas obdrzalek <obdrzalek.tom@gmail.com>
2018-07-12 00:14:49 +0100

    section services update, slider updated

--- app/js/slide.js
+++ app/js/slide.js
@@ -1,9 +1,13 @@
 const slides = document.querySelectorAll('.slide__item');
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 5500);
+let slideInterval = setInterval(nextSlide, 5000);
 
 function nextSlide(){
    slides[currentSlide].className = 'slide__item';
    currentSlide = (currentSlide+1)%slides.length;
    slides[currentSlide].classList.add('slide__item--showing');
-}
+};
+
+nextSlide();
+
+
