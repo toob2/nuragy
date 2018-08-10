93505c81c425f5a094831b4ebf7607742b47b085
tomas obdrzalek <obdrzalek.tom@gmail.com>
2018-08-10 20:38:47 +0100

    smooth_scroll_added

--- app/js/sr.js
+++ app/js/sr.js
@@ -1,2 +1,12 @@
 window.sr = ScrollReveal();
-sr.reveal('.btn', {duration: 1000}, {left: 500});
+
+const buttonSR = {
+   duration: '1500',
+   distance: '10%',
+   origin: 'top',
+   opacity: '0%'
+}
+
+sr.reveal('.btn--red', buttonSR);
+
+
